277de806877fd2a7b9745fb1172b9532d3c15620
AhnafRifqi <ahnaf.rifqi1412@gmail.com>
2024-12-13 11:17:59 +0700

    value greater than 0

--- studyCase2.java
+++ studyCase2.java
@@ -8,12 +8,14 @@ public class studyCase2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("===MENU UTAMA===");
+        while (true) {
+        System.out.println("===Main Menu===");
         System.out.println("1. Add Order");
         System.out.println("2. Display Order");
         System.out.println("3. Exit");
-        System.out.println("Choose Menu = ");
+        System.out.print("Choose Menu = ");
         int choice = sc.nextInt();
+        System.out.println();
 
         switch (choice) {
             case 1:
@@ -30,6 +32,7 @@ public class studyCase2 {
 
             default:
                 System.out.println("Invalid Choice. Re-input The Number : ");
+            }
         }
     }   
 
@@ -37,16 +40,59 @@ public class studyCase2 {
 
 public static void addOrder(){
     Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Cutomer Name: ");
+        System.out.print("Enter Cutomer Name: ");
         customerData[name][0] = sc.nextLine();
-        System.out.println("Enter Table number: ");
+        System.out.print("Enter Table number: ");
         customerData[name][1] = sc.nextLine();
+        System.out.println();
+
         menuList();  
-        name++;
-  }
+        
+        
+    int totalPrice = 0;
+    while (true) {
+        System.out.print("Choose Menu (Enter menu number, or 0 to exit): ");
+        int choice = sc.nextInt();
+
+        if (choice == 0) {
+            break;  
+        }
+
+        if (choice < 1 || choice > 4) {
+            System.out.println("Invalid menu choice, please try again.");
+            continue;
+        }
+         
+        System.out.print("Masukkan jumlah item untuk " + menu[choice - 1]);
+        int quantity = sc.nextInt();
+        totalPrice += prices[choice - 1] * quantity;
+
+        customerData[name][choice] = String.valueOf(quantity);
+        
+    }
+
+    System.out.println("Order successfully added.");
+    System.out.println("Total Prices: Rp " + totalPrice);
+
+    name++;
+    System.out.println();
+
+}
+  
 
 public static void menuList(){
+    System.out.println("=== MENU KAFE ===");
+    System.out.println("1. Kopi Hitam - Rp 15000");
+    System.out.println("2. Latte - Rp 22000");
+    System.out.println("3. Teh Tarik - Rp 12000");
+    System.out.println("4. Mie Goreng - Rp 18000");
+    System.out.println();
 
 } 
 
+
 }
+
+
+
+
